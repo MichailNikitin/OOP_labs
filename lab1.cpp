d2328e770fa168180ff82549d69149f0935d2f71
Miniki_Al <miniki2002@yandex.ua>
2022-09-03 21:51:13 +0500

    first try

---
+++ lab1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+int main() {
+   srand(time(0));
+   int n, A, B, sum, i_start, i_end;
+   bool is_pos = false;
+   sum = 0;
+   std::vector <int> arr;
+
+   std::cout<< "input size of arr - ";
+   std::cin >>n;
+   std::cout<< "input frame of arr - ";
+   std::cin >> A >> B;
+   arr.resize(n);
+
+   for (int i = 0; i < n; i++) {
+      arr[i] = (rand()%(B-A+1)+A);
+
+      if (arr[i] > 0) {
+         sum += arr[i];
+         //  это первый положительный сгенерированный элемент последовательности?
+         if (!is_pos) {
+            i_start = i;
+            std::cout<< arr[i] << "- start" << std::endl;
+            is_pos = true;
+            sum = 0;
+         }
+      }
+      else {
+         // первый отрицательный элемент после последовательности положительных элементов?
+         if (is_pos) {
+            i_end = i;
+            std::cout<< arr[i] << "- end" << std::endl;
+            is_pos = false;
+         }
+      }
+
+      if (abs(i_start - i_end) > 1 && !is_pos) {
+         double med = sum*1.0/(abs(i_start - i_end));
+         std::cout << med << "    ";
+         for (int j = i_start; j < i_end; j++)
+            if (arr[j] < med) {
+               std::cout << arr[j] << std::endl;
+               arr[j] == 0;
+            }
+         sum = 0;
+      }
+      i_end = i_start = 0;
+   }
+
+   for (int i = 0; i < n; i++)
+      std::cout << arr[i] << " ";
+
+
+
+   return 0;
+}
---
+++ temp/temp.cpp
@@ -0,0 +1,14 @@
+
+
+
+
+int main(){
+   srand(time(0));
+   int n, A, B;
+   vector<int> arr;
+
+   std::cout<< "input size of arr"
+   std::cin >>n; 
+   std::cout<< "input frame of arr"
+   std::cin >> A >> B;    
+   _abracadabra_cast(arr);
