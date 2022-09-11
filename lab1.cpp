2a72d8687aba689799919de54c20efe470e4d25a
Miniki_Al <miniki2002@yandex.ua>
2022-09-11 09:07:31 +0500

    work

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,7 @@
+// вариант №15
 #include <iostream>
 #include <vector>
-#include <random>
+#include <stdlib.h>
 
 /*
 15. Объявить массив целых чисел и заполнить его случайными значениями с помощью функций srand() и rand(). Размер массива и диапазон значений его элементов вводятся с клавиатуры. Во всех непрерывных подпоследовательностях положительных чисел заменить на 0 значения тех элементов, значения которых меньше среднего для этой подпоследовательности. Рассматриваемые подпоследовательности не должны являться частью большей подобной подпоследовательности.
@@ -15,43 +16,48 @@ int main() {
 
    std::cout<< "input size of arr - ";
    std::cin >>n;
+
    std::cout<< "input frame of arr - ";
    std::cin >> A >> B;
+
    arr.resize(n);
 
-   for (int i = 0; i < n; i++) {
+   for (int i = 0; i < n; ++i) {
       arr[i] = (rand()%(B-A+1)+A);
-
+      std::cout<< arr[i] << " ";
       if (arr[i] > 0) {
-         sum += arr[i];
          //  это первый положительный сгенерированный элемент последовательности?
          if (!is_pos) {
             i_start = i;
-            std::cout<< arr[i] << "- start" << std::endl;
             is_pos = true;
             sum = 0;
          }
+         sum += arr[i];
       }
       else {
          // первый отрицательный элемент после последовательности положительных элементов?
          if (is_pos) {
             i_end = i;
-            std::cout<< arr[i] << "- end" << std::endl;
             is_pos = false;
          }
       }
 
+      if (i == n-1 && is_pos){
+         is_pos = false;
+         i_end = i+1;
+      }
+      
       if (abs(i_start - i_end) > 1 && !is_pos) {
          double med = sum*1.0/(abs(i_start - i_end));
-         
+
          for (int j = i_start; j < i_end; j++)
-            if (arr[j] < med) 
+            if (arr[j] < med)
                arr[j] = 0;
-            
-         sum = 0;
+         i_end = i_start = 0;
       }
-      i_end = i_start = 0;
+
    }
+   std::cout << std::endl;
 
    for (int i = 0; i < n; i++)
       std::cout << arr[i] << " ";
