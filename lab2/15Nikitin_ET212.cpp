9e8ba15f5a3009f77de32255bcb8b80987869cf3
Miniki <miniki2002@yandex.ua>
2022-10-25 23:30:42 +0500

    2

--- lab2/15Nikitin_ET212.cpp
+++ lab2/15Nikitin_ET212.cpp
@@ -7,13 +7,17 @@ class Polar {
    double phi;
 public:
    Polar(double = 1, double = 0);
-   Polar &operator+=(const Polar&); //поворот вектора на d градусов против часовой стрелке
-   Polar &operator-=(const Polar&); //поворот вектора на d градусов по часовой стрелке
-   Polar &operator*=(const Polar&);
-   Polar &operator/=(const Polar&);
-
-
-
+   Polar &operator+=(const Polar &); //поворот вектора на d градусов против часовой стрелке
+   Polar &operator-=(const Polar &); //поворот вектора на d градусов по часовой стрелке
+   Polar &operator*=(const Polar &); //увеличение вектора в d раз
+   Polar &operator/=(const Polar &); //уменьшение вектора в d раз
+   friend bool operator==(const Polar &, const Polar &); //перегрузка операции ==
+   friend bool operator!=(const Polar &, const Polar &); //перегрузка операции !=
+   bool operator!()const {return (a==0);}//перегрузка операции !
+   friend istream &operator>>(istream &, Polar &);//перегрузка операции  >>
+   friend ostream &operator<<(ostream &, const Polar &);// перегрузка операции  <<
+   double getLength() const {return r}; // радиус
+   double getAngle() const {return phi}; // угол
 }
 
 
@@ -24,27 +28,35 @@ int main() {
 
 }
 
-Polar::Polar(double r, double phi){
+Polar::Polar(double r, double phi) {
    this->r = r;
    this->phi = phi%360;
 }
 
-Polar &Polar::operator+=(const Polar&d){
+Polar &Polar::operator+=(const Polar &d) {
    phi = (phi+d.phi)%360;
    return *this;
 }
 
-Polar &Polar::operator-=(const Polar&d){
+Polar &Polar::operator-=(const Polar &d) {
    phi = (phi-d.phi)%360;
    return *this;
 }
 
-Polar &Polar::operator*=(const Polar&d){
+Polar &Polar::operator*=(const Polar &d) {
    r = d.r;
    return *this;
 }
 
-Polar &Polar::operator/=(const Polar&d){
+Polar &Polar::operator/=(const Polar &d) {
    phi = d.phi%360;
    return *this;
+}
+
+bool operator==(const Polar &x, const Polar &y) {
+   return ((x.a==x.b&&y.a==y.b);
+}
+
+bool operator!=(const Polar &x, const Polar &y) {
+   return !(x==y);
 }
